5fc76d070bdbda694f63e35d4dcb824ec7720b64
sin <379457721@qq.com>
2017-08-08 20:45:12 +0800

    Update RNVideoTrimmerBridge.m

--- ios/RNVideoProcessing/RNVideoTrimmer/RNVideoTrimmerBridge.m
+++ ios/RNVideoProcessing/RNVideoTrimmer/RNVideoTrimmerBridge.m
@@ -14,5 +14,7 @@ RCT_EXTERN_METHOD(trim:(NSString *)source options:(NSDictionary *)options callba
 RCT_EXTERN_METHOD(compress:(NSString *)source options:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback);
 RCT_EXTERN_METHOD(getPreviewImageAtPosition:(NSString *)source atTime:(float *)atTime maximumSize:(NSDictionary *)maximumSize format:(NSString *)format callback:(RCTResponseSenderBlock)callback);
 RCT_EXTERN_METHOD(crop:(NSString *)source options:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback);
+RCT_EXTERN_METHOD(getPreviewImages:(NSString *)source  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(getVideoInfo:(NSString *)source  resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject);
 
 @end
